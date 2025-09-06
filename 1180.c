d915dd062813e0508327dd7686ad2ac3c41ded3e
Julia dos Santos Mota <julia.mota@unifesp.br>
2025-09-05 21:25:59 -0300

    AED1

--- 1180.c
+++ 1180.c
@@ -1,6 +1,6 @@
 /* 1180 Beecrowd: Faça um programa que leia um valor N. Este N será o tamanho de um vetor X[N].
 * A seguir, leia cada um dos valores de X, encontre o menor elemento deste vetor
-e a sua posição dentro do vetor, mostrando esta informação. */
+* e a sua posição dentro do vetor, mostrando esta informação. */
 
 #include <stdio.h>
 
