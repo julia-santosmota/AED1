ff284caa1033f6338917145e7376dba0e3e1a069
Julia dos Santos Mota <julia.mota@unifesp.br>
2025-11-11 19:58:02 -0300

    AED1

---
+++ 1197.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int v, t;
+    while (cin >> v >> t) {
+        int deslocamento = 2 * v * t;
+        cout << deslocamento << endl; 
+    }
+    return 0;
+}
