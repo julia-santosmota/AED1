40bd2e98fec06f372456575b1bda3ac10bf93b3a
Julia dos Santos Mota <julia.mota@unifesp.br>
2025-11-20 13:21:08 -0300

    AED1

---
+++ 1082a.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <string.h>
+
+int grafo[26][26];
+int visitado[26];
+char componente[26];
+int tamanho_componente;
+
+void dfs(int v, int total_vertices) {
+    visitado[v] = 1;
+    componente[tamanho_componente++] = 'a' + v;
+    for (int u = 0; u < total_vertices; u++) {
+        if (grafo[v][u] && !visitado[u]) {
+            dfs(u, total_vertices);
+        }
+    }
+}
+
+void ordenar(char *vetor, int tamanho) {
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = i + 1; j < tamanho; j++) {
+            if (vetor[i] > vetor[j]) {
+                char temp = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int N;
+    scanf("%d", &N);
+
+    for (int caso = 1; caso <= N; caso++) {
+        int V, E;
+        scanf("%d %d", &V, &E);
+
+        memset(grafo, 0, sizeof(grafo));
+        memset(visitado, 0, sizeof(visitado));
+
+        for (int i = 0; i < E; i++) {
+            char a, b;
+            scanf(" %c %c", &a, &b);
+            int u = a - 'a';
+            int v = b - 'a';
+            grafo[u][v] = 1;
+            grafo[v][u] = 1; 
+        }
+
+        printf("Case #%d:\n", caso);
+
+        int componentes_conectados = 0;
+        for (int i = 0; i < V; i++) {
+            if (!visitado[i]) {
+                tamanho_componente = 0;
+                dfs(i, V);
+                ordenar(componente, tamanho_componente);
+                for (int j = 0; j < tamanho_componente; j++)
+                    printf("%c,", componente[j]);
+                printf("\n");
+                componentes_conectados++;
+            }
+        }
+        printf("%d connected components\n\n", componentes_conectados);
+    }
+
+    return 0;
+}
